59974dd927908466806e85fd6f797639cb208477
lyss <147207363+Cherry-Trees@users.noreply.github.com>
2025-09-03 13:47:01 -0500

    Create main.cpp

---
+++ knapsack/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int M[101][101];
+    memset(M[0], 0, 101);
+  
+    int N, W;
+    int w[101];
+    int v[101];
+  
+    while (cin >> W >> N) {
+        for (int i = 0; i < N; ++i)
+            cin >> v[i + 1] >> w[i + 1];
+        
+        for (int i = 1; i <= N; ++i) {
+            for (int j = 0; j <= W; ++j) {
+                if (w[i] > j)
+                    M[i][j] = M[i - 1][j];
+                else
+                    M[i][j] = max(M[i - 1][j], v[i] + M[i - 1][j - w[i]]);
+            }
+        }
+
+        int c = W;
+        int a = 0;
+        bool p[2000];
+        memset(p, 0, 2000);
+        for (int i = N; i >= 1; --i)
+            if (M[i][c] != M[i - 1][c]) {
+                c -= w[i];
+                p[i - 1] = 1;
+                ++a;
+            }
+
+        cout << a << endl;
+        for (int i = 0; i < N; ++i)
+            if (p[i])
+                cout << i << ' ';
+    }
+  
+    return 0;
+}
